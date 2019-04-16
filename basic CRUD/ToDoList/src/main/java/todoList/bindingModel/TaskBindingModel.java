34ed53c02b40257cce46c586d5cbcb8713a8bcd2
VladimirDGeorgiev <vladogeorgiev222@abv.bg>
2019-04-16 07:20:03 +0300

    basic web and CRUD

---
+++ basic CRUD/ToDoList/src/main/java/todoList/bindingModel/TaskBindingModel.java
@@ -0,0 +1,14 @@
+package todoList.bindingModel;
+
+public class TaskBindingModel {
+    private String title;
+    private String comments;
+
+    public String getTitle() { return this.title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public String getComments() { return this.comments; }
+
+    public void setComments(String comments) { this.comments = comments; }
+}
---
+++ basic CRUD/meistertask/src/main/java/meisterTask/entities/Task.java
@@ -0,0 +1,50 @@
+package meisterTask.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String status;
+
+    public Task() {
+    }
+
+    public Task(Integer id, String title, String status) {
+        this.id = id;
+        this.title = title;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
