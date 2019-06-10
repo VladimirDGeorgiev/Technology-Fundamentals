0f53761e87a906a7705f46a836fec55b0b19cb07
VladimirDGeorgiev <vladogeorgiev222@abv.bg>
2019-06-10 11:59:23 +0300

    ExamPreparation_JavaAdvanced/Archive_AdvancedJava_Exams/JavaAdvancedRetakeExam_27August2018

---
+++ ExamPreparation_JavaAdvanced/Archive_AdvancedJava_Exams/JavaBasicsExam_3September2014/p02AddingAngles.java
@@ -0,0 +1,11 @@
+package ExamPreparation_JavaAdvanced.Archive_AdvancedJava_Exams.JavaBasicsExam_3September2014;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class p02AddingAngles {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
